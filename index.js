b406003d0df8765a2709b98dc042f812d99de168
samuelpinheiro <samuel.pinheiro.senai564@gmail.com>
2024-05-08 14:50:58 -0300

    feat: Adicionar rotas para heróis e batalhas

--- index.js
+++ index.js
@@ -18,14 +18,22 @@ app.listen(PORT, () => {
   console.log(`funcionando normalmente ${PORT}🚀`);
 });
 
-app.get("/", async (req, res) => {
+app.get("/heros", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM heros;");
+    const result = await pool.query("SELECT * FROM heros");
     res.json(result.rows);
   } catch (err) {
     console.log(err);
   }
 });
+app.get("/", async (req, res) => {
+    try {
+        res.json({ message: "Bem-Vindo a batalha de herois." });
+    } catch (err) {
+        console.log(err);
+    }
+    });
+    
 app.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -35,25 +43,15 @@ app.get("/:id", async (req, res) => {
     console.log(err);
   }
 });
-app.get("/name/:name", async (req, res) => {
-  const { name } = req.params;
-  try {
-    const result = await pool.query(`SELECT * FROM heros WHERE name =$1`, [
-      name,
-    ]);
-    res.json(result.rows);
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.post("/", async (req, res) => {
+
+app.post("/heros", async (req, res) => {
   const { name, power_values, power_type, hp, attack } = req.body;
   try {
-    const result = await pool.query(
+   await pool.query(
       `INSERT INTO heros (name, power_values, power_type, hp , attack ) VALUES ($1, $2,$3,$4,$5)`,
       [name, power_values, power_type, hp, attack]
     );
-    res.json(result.rows);
+    res.json({ message: "Heroi cadastrado com sucesso" });
   } catch (err) {
     console.log(err);
   }
@@ -76,6 +74,8 @@ app.put("/:id", async (req, res) => {
 app.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
+   await pool.query(`DELETE FROM batalhas WHERE heros_p = $1`, [id]);
+    await pool.query(`DELETE FROM batalhas WHERE heros_s = $1`, [id]);
     const result = await pool.query(`DELETE FROM heros WHERE id = $1`, [id]);
     res.json(result.rows);
   } catch (err) {
@@ -83,23 +83,43 @@ app.delete("/:id", async (req, res) => {
   }
 });
 
-app.post("/batalha/:id1/:id2", async (req, res) => {
-    const { id1, id2 } = req.params;
-    const batalha_id = Math.floor(Math.random() * 1000);
-    try {
-        await pool.query(`INSERT INTO batalhas (id,heros_p, heros_s) VALUES ($1,$2,%3)`, [batalha_id,id1, id2]);
-        await pool.query(`SELECTFROM obter_vencedor_batalha($1) `, [batalha_id]);
-        res.json({ winner, looser });
-    } catch (err) {
-      console.log(err);
-    }
-});
-
-app.get("/batalhas", async (req, res) => {
+app.get("/batalha/:id1/:id2", async (req, res) => {
+  const { id1, id2 } = req.params;
+  const batalha_id = Math.floor(Math.random() * 1000);
+  let winner;
   try {
-    const result = await pool.query(`SELECT * FROM batalhas`);
-    res.json(result.rows);
+      await pool.query(`INSERT INTO batalhas (id, heros_p, heros_s) VALUES ($1, $2, $3)`, [batalha_id, id1, id2]);
+      const result = await pool.query(`SELECT obter_vencedor_batalha($1) AS winner`, [batalha_id]);
+      winner = result.rows[0].winner;
+
+      // Consulta para obter os dados do vencedor usando o nome do vencedor
+      const winnerDataResult = await pool.query(`SELECT * FROM heros WHERE name = $1`, [winner]);
+      const winnerData = winnerDataResult.rows[0];
+
+      res.json({ message: `Vencedor da batalha foi ${winner}`, winnerData });
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.get("/heros/batalhas", async (req, res) => {
+  try {
+      // Consulta para obter o histórico de batalhas
+      const historicoResult = await pool.query(`SELECT * FROM batalhas`);
+      const historico = historicoResult.rows;
+
+      // Para cada batalha no histórico, obter os detalhes dos heróis
+      for (let i = 0; i < historico.length; i++) {
+          const batalha = historico[i];
+          const heroiPResult = await pool.query(`SELECT * FROM heros WHERE id = $1`, [batalha.heros_p]);
+          const heroiSResult = await pool.query(`SELECT * FROM heros WHERE id = $1`, [batalha.heros_s]);
+          batalha.heroi_p = heroiPResult.rows[0];
+          batalha.heroi_s = heroiSResult.rows[0];
+      }
+
+      res.json({ historico });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Erro ao obter o histórico de batalhas" });
   }
 });
